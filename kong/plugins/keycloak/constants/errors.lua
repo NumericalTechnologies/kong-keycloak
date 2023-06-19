34871b37053bb2fd094157605c6a0c2d11c40d82
Prakoso Santoso <prakoso.santoso@numtech.com>
2023-06-19 16:49:16 +0800

    Add roles to error messages

--- kong/plugins/keycloak/constants/errors.lua
+++ kong/plugins/keycloak/constants/errors.lua
@@ -1,8 +1,8 @@
 return {
   USERNAME_LENGTH_ERROR_MESSAGE = "Usernames should be between 1 and 64 characters.",
   USERNAME_ASCII_ONLY_ERROR_MESSAGE = "Usernames should only contain ASCII characters.",
-  PROTECTED_USER_DELETE_ERROR_MESSAGE = "Cannot delete a protected user.",
-  PROTECTED_USER_MODIFY_ERROR_MESSAGE = "Cannot modify a protected user.",
+  PROTECTED_USER_DELETE_ERROR_MESSAGE = "Cannot delete a protected user or its roles.",
+  PROTECTED_USER_MODIFY_ERROR_MESSAGE = "Cannot modify a protected user or its roles.",
   MISSING_AUTHORIZATION_HEADER_ERROR_MESSAGE = "Missing authorization header.",
   UNEXPECTED_ERROR_MESSAGE = "An unexpected error has occurred.",
   USER_LIMIT_REACHED_ERROR_MESSAGE = "You have reached the maximum number of users allowed."
